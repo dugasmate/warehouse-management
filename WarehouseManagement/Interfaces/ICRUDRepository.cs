a6a0068442a9602e9c88cb691ce807ed556e264b
dugasmate <dugasmate@gmail.com>
2018-11-21 00:43:30 +0100

    add interface for repositories

---
+++ WarehouseManagement/Interfaces/ICRUDRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarehouseManagement.Interfaces
+{
+    interface Interface
+    {
+    }
+}
