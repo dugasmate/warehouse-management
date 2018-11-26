d091a7960fe020632844ea32b9b0040a41eef9a6
dugasmate <dugasmate@gmail.com>
2018-11-26 23:49:56 +0100

    add environment variables and seperate environment for in memory databas

--- WarehouseManagement/Startup.cs
+++ WarehouseManagement/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using WarehouseManagement.Configurations;
 using WarehouseManagement.Context;
@@ -14,12 +16,31 @@ namespace WarehouseManagement
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json", optional: true);
+
+            Configuration = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
             services.AddInjections();
-            services.AddDbContext<WarehouseContext>(options =>
-  options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Warehouse;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
+            var connection = Configuration["ConnectionStrings:DefaultConnection"];
+            services.AddDbContext<WarehouseContext>(options => options.UseSqlServer(connection));
+        }
+
+        public void ConfigureTestServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddInjections();
+            var connection = Configuration["ConnectionStrings:DefaultConnection"];
+            services.AddDbContext<WarehouseContext>(options => options.UseInMemoryDatabase("WarehouseDB"));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
